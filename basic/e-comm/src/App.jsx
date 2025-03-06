f30a1341536abbfe8ef86e7922dc9e78b92ec4a2
manpreet7876 <103956751+manpreet7876@users.noreply.github.com>
2025-03-06 16:09:23 +0530

    ecom updation

--- basic/e-comm/src/App.jsx
+++ basic/e-comm/src/App.jsx
@@ -1,20 +1,23 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Login from "./Login"
-import ProductCatalog from './ProductCatalog'
-import './App.css'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import ProductCatalog from "./ProductCatalog";
+import "./App.css";
+import CartPage from "./CartPage";
+import PaymentPage from "./PaymentPage";
+
 
 function App() {
-  
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/products" element={<ProductCatalog />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/payment" element={<PaymentPage />} />
       </Routes>
     </Router>
-
-  )
+  );
 }
 
-export default App
+export default App;
